d8bb2873c5b7f7d2f94ec2e834b556212e28fb68
Carlos Durán <cduran@plancomunicacion.com>
2020-02-20 17:34:38 +0100

    First commit

---
+++ components/TheNavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav class="nav">
+    <div class="logo">
+      <nuxt-link :to="{name: 'index'}" class="logo text-lg">
+        Nuxt Fundamentals
+      </nuxt-link>
+      <a href="#">
+        
+      </a>
+
+      <span class="subheader">A Vue school course</span>
+    </div>
+  </nav>
+</template>
+
+<script>
+  export default {
+    
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <TheNavBar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import TheNavBar from '@/components/TheNavBar'
+export default {
+  components: {
+    TheNavBar
+  }
+}
+</script>
+
+<style>
+
+</style>
